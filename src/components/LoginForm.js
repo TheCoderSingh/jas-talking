5bbbf40c2b471b2a1287773590d4825eac0660eb
Jaskaran Singh <jaskaran1523@gmail.com>
2021-09-03 23:01:23 -0700

    Create Login page and Login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,18 @@
 import React from "react";
+import "./LoginForm.scss";
+import { UserOutlined, UnlockFilled } from "@ant-design/icons";
 
 const LoginForm = () => (
 	<div id="login-form">
 		<form>
-			<label htmlFor="username">
-				Username
+			<div className="input-container">
+				<UserOutlined />
 				<input placeholder="Username" id="username" />
-			</label>
-			<label htmlFor="password">
-				Password
-				<input type="password" placeholder="Username" id="password" />
-			</label>
+			</div>
+			<div className="input-container">
+				<UnlockFilled />
+				<input type="password" placeholder="Password" id="password" />
+			</div>
 			<button type="submit">Login</button>
 		</form>
 	</div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,11 @@
 import React from "react";
+import LoginForm from "../components/LoginForm";
 import "./Login.scss";
 
 const Login = () => (
 	<div id="login">
-		<p>Login</p>
+		<h1>Sign In</h1>
+		<LoginForm />
 	</div>
 );
 
